ee9fe3befc9f173b2afc53b4de8556e5463a51e4
Adán Aguilar <adandaniel99@hotmail.es>
2020-09-13 19:16:06 -0500

    version 1

---
+++ A2U2 PW/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using A2U2_PW.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace A2U2_PW.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            mexicoContext context = new mexicoContext();
+            var estados = context.Estados.OrderBy(x => x.Nombre).ToList();
+            return View(estados);
+        }
+    }
+}
---
+++ A2U2 PW/Models/Estados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace A2U2_PW.Models
+{
+    public partial class Estados
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Abrev { get; set; }
+    }
+}
